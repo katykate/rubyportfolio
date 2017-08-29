37753eb433cd3a4a90ebe69011abad663877927d
Katy Kate <krawczykate@gmail.com>
2017-08-29 22:38:44 +0100

    3.11 routesrb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
+  get '/about'
 
-  get 'static_pages/contact'
+  get '/contact'
 
-  get 'static_pages/index'
-  root 'static_pages#index'
+  get '/index'
+  root '#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
