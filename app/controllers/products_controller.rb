1d530d0ffa5f454febde1e34566dfacae39071da
Katy Kate <krawczykate@gmail.com>
2017-09-04 19:00:10 +0100

    4.4

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -71,5 +71,5 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:name, :description, :image_url, :color)
   end
-  
+
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,15 @@
 class StaticPagesController < ApplicationController
   def index
   end
+  
   def about
   end
+
   def contact
   end
+
+  def landing_page
+    @featured_product = Product.first
+  end
+
 end
